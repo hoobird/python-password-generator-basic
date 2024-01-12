d10c59674258b57b5ce930bc22913bbe78341563
hoobird <111221196+hoobird@users.noreply.github.com>
2024-01-12 02:03:17 +0000

    im done

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 # Write you random password generator program here!
+import random
+
+def generate_password():
+  characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!\"#$%&'()*+, -./:;<=>?@[\\]^_`{|}~"
+  
+  password_length = int(input("Enter the password length: "))
+  password = ''.join(random.choice(characters) for i in range(password_length))
+  
+  print(f"Your generated password is: {password}")
+
+generate_password()
